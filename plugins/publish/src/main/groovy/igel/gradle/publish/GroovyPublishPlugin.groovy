6e55d4e2e9412e976cd2021d8c00a92a1baf5151
Pavel Stepanov <stefan.nsk@gmail.com>
2016-03-30 01:46:56 +0600

    add igel.publish.gradle plugin

---
+++ plugins/publish/src/main/groovy/igel/gradle/publish/GroovyPublishPlugin.groovy
@@ -0,0 +1,16 @@
+package igel.gradle.publish
+
+import org.gradle.api.GradleException
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GroovyPublishPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project target) {
+        if (!target.plugins.hasPlugin('groovy')) {
+            throw new GradleException("plugin 'groovy' should be applied first")
+        }
+    }
+
+}
