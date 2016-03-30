89ff65fcca1e9b55d49c65401c49328dbe9344cf
Pavel Stepanov <stefan.nsk@gmail.com>
2016-03-31 00:20:00 +0600

    implement 'igel.publish.groovy'

--- plugins/publish/src/main/groovy/igel/gradle/publish/GroovyPublishPlugin.groovy
+++ plugins/publish/src/main/groovy/igel/gradle/publish/GroovyPublishPlugin.groovy
@@ -3,6 +3,9 @@ package igel.gradle.publish
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.tasks.bundling.Jar
 
 class GroovyPublishPlugin implements Plugin<Project> {
 
@@ -11,6 +14,20 @@ class GroovyPublishPlugin implements Plugin<Project> {
         target.afterEvaluate {
             if (!target.plugins.hasPlugin('groovy')) {
                 throw new GradleException("plugin 'groovy' should be applied")
+            }
+
+            target.apply plugin: 'maven-publish'
+
+            Task sourcesJarTask = target.task('sourcesJar', type: Jar) {
+                group = 'documentation'
+                description = 'Assembles a jar archive containing the main source code.'
+                classifier = 'sources'
+                from target.sourceSets.main.allSource
+            }
+
+            target.publishing.publications.create('maven', MavenPublication) {
+                from target.components.java
+                artifact sourcesJarTask
             }
         }
     }
