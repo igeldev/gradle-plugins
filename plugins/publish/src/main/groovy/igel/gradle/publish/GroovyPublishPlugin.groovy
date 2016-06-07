d96276130b9bb655bd0704eb2889a9603fc8d9a6
Pavel Stepanov <stefan.nsk@gmail.com>
2016-06-08 00:24:03 +0600

    Re-Implement GroovyPublishPlugin : use NewBasePublishPlugin as superclass.

--- plugins/publish/src/main/groovy/igel/gradle/publish/GroovyPublishPlugin.groovy
+++ plugins/publish/src/main/groovy/igel/gradle/publish/GroovyPublishPlugin.groovy
@@ -17,46 +17,73 @@
 package igel.gradle.publish
 
 import org.gradle.api.Action
-import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.XmlProvider
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.Jar
 import org.gradle.api.tasks.javadoc.Groovydoc
+import org.gradle.jvm.tasks.Jar
 
-class GroovyPublishPlugin extends BasePublishPlugin {
+class GroovyPublishPlugin extends NewBasePublishPlugin<NewBasePublishPlugin.Extension> {
 
-    @Override
-    protected Action<MavenPublication> getMavenConfiguration(Project target) {
-        // check that 'groovy' plugin is applied
-        // because we need 'java' component, 'main' source set and groovydoc task
-        if (!target.plugins.hasPlugin('groovy')) {
-            throw new GradleException('plugin \'groovy\' should be applied first')
-        }
+    GroovyPublishPlugin() {
+        super('groovy',
+                Extension, 'publishGroovy',
+                'groovy')
+    }
 
-        // create a task to prepare source code jar
-        Task sourcesJarTask = target.task('sourcesJar', type: Jar) {
+    private static DefaultPublishArtifact prepareArtifact(Project project) {
+        Jar jarTask = project.tasks['jar'] as Jar
+        File jarFile = jarTask.archivePath
+        return new DefaultPublishArtifact(tasks: [jarTask], extension: 'jar', file: jarFile)
+    }
+
+    private static File findPomContentFile(Project project) {
+        List<File> files = [
+                project.file("src/main/$POM_CONTENT_FILENAME"),
+        ]
+        return files.find { it.exists() }
+    }
+
+    private Task createSourcesTask(Project project) {
+        return project.task("sourcesJar", type: Jar) {
             group = 'documentation'
             description = 'Assembles a jar archive containing the main source code.'
             classifier = 'sources'
-            from target.sourceSets.main.allSource
+            from project.sourceSets.main.allSource
         }
+    }
 
-        // create a task to prepare groovydoc jar
-        Groovydoc groovydocTask = target.tasks['groovydoc'] as Groovydoc
-        Task groovydocJarTask = target.task('groovydocJar', type: Jar,
-                dependsOn: groovydocTask) {
+    private Task createGroovydocTask(Project project) {
+        Groovydoc groovydocTask = project.tasks['groovydoc'] as Groovydoc
+        return project.task('groovydocJar', type: Jar, dependsOn: groovydocTask) {
             group = 'documentation'
             description = 'Assembles a jar archive containing the Groovydoc API documentation.'
             classifier = 'groovydoc'
             from groovydocTask.destinationDir
         }
+    }
 
-        // return maven configuration
+    @Override
+    protected Action<MavenPublication> prepareMavenConfiguration(Project project) {
+        Task sourcesTask = createSourcesTask(project)
+        Task groovydocTask = createGroovydocTask(project)
+
+        Action<? super XmlProvider> pomConfiguration = createPomConfiguration(
+                project, findPomContentFile(project),
+                project.configurations.compile.allDependencies)
+
         return { MavenPublication publication ->
-            publication.from target.components.java
-            publication.artifact sourcesJarTask
-            publication.artifact groovydocJarTask
+            publication.artifact prepareArtifact(project)
+
+            if (extension.publishSrc) {
+                publication.artifact sourcesTask
+            }
+            if (extension.publishDoc) {
+                publication.artifact groovydocTask
+            }
+
+            publication.pom.withXml pomConfiguration
         }
     }
 
