2b3f958c68360aa284b28492ebed3f30895d1a9d
Pavel Stepanov <stefan.nsk@gmail.com>
2016-07-12 01:11:19 +0600

    Add 'OK' and 'Interrupt' buttons.

--- plugins/common/src/main/groovy/igel/gradle/common/InputDialog.groovy
+++ plugins/common/src/main/groovy/igel/gradle/common/InputDialog.groovy
@@ -2,24 +2,76 @@ package igel.gradle.common
 
 import javax.swing.*
 import java.awt.*
-import java.awt.event.KeyEvent
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
+import java.awt.event.*
 
 class InputDialog extends JDialog {
 
     private final Object lock = new Object()
 
     private JPanel contentPane
+    private JPanel inputPane
+    private JButton buttonOK
+    private JButton buttonInterrupt
+
+    private void setup(Input input) {
+        this.contentPane = new JPanel()
+        contentPane.layout = new GridBagLayout()
+
+        // input pane
+
+        this.inputPane = new JPanel()
+        // todo add input UI here into inputPane
+
+        contentPane.add(inputPane, new GridBagConstraints(
+                gridwidth: GridBagConstraints.REMAINDER,
+                fill: GridBagConstraints.BOTH,
+                weightx: 1.0, weighty: 1.0))
+
+        // button space
+
+        JLabel buttonSpace = new JLabel()
+
+        contentPane.add(buttonSpace, new GridBagConstraints(
+                gridwidth: 1, weightx: 1.0))
+
+        // button ok
+
+        this.buttonOK = new JButton()
+        buttonOK.text = 'OK'
+
+        contentPane.add(buttonOK, new GridBagConstraints(
+                gridwidth: 1, insets: new Insets(4, 8, 4, 8)))
+
+        // button interrupt
+
+        this.buttonInterrupt = new JButton()
+        buttonInterrupt.text = 'Interrupt'
+
+        contentPane.add(buttonInterrupt, new GridBagConstraints(
+                gridwidth: 1, insets: new Insets(4, 8, 4, 8)))
+    }
 
     InputDialog(Input input) {
         this.title = input.title
 
-        this.contentPane = new JPanel()
-        contentPane.setBackground(Color.LIGHT_GRAY)
-
+        setup(input)
 
         setContentPane(contentPane)
+        getRootPane().setDefaultButton(buttonOK)
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // todo save input values here
+                hideUI()
+            }
+        })
+
+        buttonInterrupt.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // todo fail build
+                hideUI()
+            }
+        })
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE)
         addWindowListener(new WindowAdapter() {
