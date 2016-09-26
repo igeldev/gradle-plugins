4c60b34d09e804747cf536b1ae6765de9aa839e9
Pavel Stepanov <stefan.nsk@gmail.com>
2016-09-27 01:51:35 +0700

    Draft Checkstyle for Java.

--- plugins/check/src/main/groovy/igel/gradle/check/JavaCheckPlugin.groovy
+++ plugins/check/src/main/groovy/igel/gradle/check/JavaCheckPlugin.groovy
@@ -20,6 +20,9 @@ import igel.gradle.check.methods.AbstractCheckMethod
 import igel.gradle.check.methods.Method1
 import igel.gradle.check.methods.Method2
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.SourceDirectorySet
 
 class JavaCheckPlugin extends BaseCheckPlugin<JavaCheckPlugin, Extension> {
 
@@ -42,8 +45,56 @@ class JavaCheckPlugin extends BaseCheckPlugin<JavaCheckPlugin, Extension> {
         return [new Method1(project), new Method2(project)]
     }
 
+    static File copyResource(Project project, String resourcePath, File outputFile) {
+        ClassLoader loader = project.buildscript.classLoader
+
+        outputFile.delete()
+        outputFile.parentFile.mkdirs()
+        outputFile << loader.getResourceAsStream(resourcePath).text
+        return outputFile
+    }
+
+    private Configuration configurationCheckstyle
+
+    private void prepareCheckstyle(Project project) {
+        configurationCheckstyle = project.configurations.create('checkCheckstyle')
+        configurationCheckstyle.description = 'Checkstyle dependencies.'
+        configurationCheckstyle.visible = false
+
+        configurationCheckstyle.defaultDependencies { dependencies ->
+            dependencies.add(project.dependencies.create('com.puppycrawl.tools:checkstyle:6.19'))
+        }
+    }
+
+    private void performCheckstyle(Project project) {
+        project.ant.taskdef(resource: 'com/puppycrawl/tools/checkstyle/ant/checkstyle-ant-task.properties') {
+            classpath {
+                configurationCheckstyle.resolve().each {
+                    pathelement(location: it.absolutePath)
+                }
+            }
+        }
+
+        File configFile = copyResource(project, 'configCheckstyle/config.xml',
+                project.file('build/check/checkstyle/config.xml'))
+        SourceDirectorySet sources = project.sourceSets.main.java
+        if (sources.findAll { it.exists() }.empty) {
+            return
+        }
+        project.ant.checkstyle(config: configFile, failOnViolation: false) {
+            formatter(type: 'xml', toFile: project.file('build/check/checkstyle/report.xml'))
+            sources.getSrcDirs().each { fileset(dir: it.absolutePath) }
+        }
+    }
+
     @Override
     protected void doApply(Project project) {
+        Task checkTestTask = project.task('check-test')
+        project.afterEvaluate { project.tasks['check'].dependsOn checkTestTask }
+
+        // Checkstyle
+        prepareCheckstyle(project)
+        checkTestTask << { performCheckstyle(project) }
     }
 
 }
